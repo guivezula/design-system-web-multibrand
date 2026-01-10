78539753ea17706bf354d738bae76ce33aa4c259
Guilherme Vezula <vezula18@gmail.com>
2026-01-10 08:26:56 -0300

    add bases into index

--- src/mixins/index.ts
+++ src/mixins/index.ts
@@ -1,5 +1,7 @@
 export * from "./base-button";
 export * from "./base-component";
+export * from "./base-input";
+export * from "./base-input-text";
 export * from "./base-loading";
 export * from "./base-slot";
 
